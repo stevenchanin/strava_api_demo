f7894968deca2e5e3178e7a29d3938e73464565d
Steven Chanin <steven_chanin@alum.mit.edu>
2010-09-23 15:31:04 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StravaDemo::Application.config.secret_token = 'ae4299fad7c5f3a811380091299d7bfc99aaed5fcda9adc75d415459bcb74682727d233215062051df799f280693ffbc6b2afecd711d17ebbea9972472784a0c'
